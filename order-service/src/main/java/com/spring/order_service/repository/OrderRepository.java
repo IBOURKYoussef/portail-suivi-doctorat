00fffec338c44e79d9b5d03d8884f8877f3a03ff
IBOURK Youssef <Youssef.Ibourk@emsi-edu.ma>
2026-01-05 21:15:32 +0100

    Delete order-service directory

--- order-service/src/main/java/com/spring/order_service/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.order_service.repository;
-
-import com.spring.order_service.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface
-
-OrderRepository extends JpaRepository<Order, Long> {
-}
